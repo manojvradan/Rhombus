236f1376685d768bff6bc06b63c7177e291933eb
Manojvradan <manojvradanbs@gmail.com>
2025-11-23 14:17:40 +1100

    Vercel-Config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-change-me-before-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
---
+++ backend/vercel_app.py
@@ -0,0 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+# CHANGE 'core' to the name of the folder containing settings.py
+# Based on your image, it looks like 'core' or 'api' contains settings.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+app = get_wsgi_application()
